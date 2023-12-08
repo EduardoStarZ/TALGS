5714842728dfc52358be65bd7911dac75cd4122b
EduardoStarZ <pathver@gmail.com>
2023-12-08 04:17:29 -0300

    finished first page front

--- pages/views.py
+++ pages/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 
 
 def main(request):
-    template = 'index.html'
+    template = 'home.html'
     context = {}
     
     return render(request, template, context)
