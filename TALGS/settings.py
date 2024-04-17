2ee542c9568f8310b4fdd31e556393e857c3b030
EduardoStarZ <pathver@gmail.com>
2024-04-16 21:29:11 -0300

    basically finished login page, with 95% functionality

--- TALGS/settings.py
+++ TALGS/settings.py
@@ -130,5 +130,5 @@ STATIC_ROOT = BASE_DIR / 'cache'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-LOGIN_REDIRECT_URL = '/admin'
-LOGOUT_REDIRECT_URL = '/'
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = 'auth/login/'
