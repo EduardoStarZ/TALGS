1247f5966147f236695c8c1ddab680682471eb67
EduardoStarz <pathver@gmail.com>
2024-06-17 17:18:28 -0300

    finished parser completely

--- src/parser.rs
+++ src/parser.rs
@@ -1,4 +1,4 @@
-pub fn hex_str_spaced_to_u8_vec(str : String) -> Vec<u8> {
+pub fn hex_str_spaced_to_u8_vec(str : &String) -> Vec<u8> {
     str.trim_start_matches(" ").trim_end_matches(" ").split(" ").map(|x| match u8::from_str_radix(x, 16) {
         Ok(value) => value,
         Err(err) => {
@@ -9,34 +9,29 @@ pub fn hex_str_spaced_to_u8_vec(str : String) -> Vec<u8> {
         ).collect::<Vec<u8>>()
 }
 
-pub fn unspaced_hex_str_to_u8_vec(str : String) -> Vec<u8> {
+pub fn unspaced_hex_str_to_u8_vec(str : &String) -> Vec<u8> {
     let mut hexes : Vec<u8> = Vec::new();
-    let mut chars : String = String::new();
+    let mut copied_str = str.clone().chars().rev().collect::<String>();
 
-    for (i, x) in str.chars().enumerate() {
-        chars.push(x);
+    for _ in 0..(str.len() / 2) {
+        let sub_str : String = format!("{}{}", copied_str.pop().unwrap(), copied_str.pop().unwrap());
 
-        if i+1 % 2 == 0 {
-            chars = String::new();
-
-            hexes.push(
-                match u8::from_str_radix(&(*chars), 16) {
-                    Ok(value) => value,
-                    Err(err) => {
-                        eprintln!("Error while parsing a hexadecimal value to a unsigned integer of 8 bits! Returning a u8::MIN value and displaying backtrace: {err}"); 
-                        u8::MIN 
-                    }
-                }
-            )
-        }
+        let value : u8 = match u8::from_str_radix(sub_str.as_str(), 16) {
+            Ok(value) => value,
+            Err(err) => {
+                eprintln!("Error while parsing a hexadecimal value to a unsigned integer of 8 bits! Returning a u8::MIN value and displaying backtrace: {err}"); 
+                u8::MIN 
+            }
+        };
+        hexes.push(value);
     }
     return hexes;
 }
 
-pub fn u8_vec_to_hex_str(vector : Vec<u8>) -> String {
-    vector.iter().map(|x| format!("{x:0>2b }")).collect::<String>()
+pub fn u8_vec_to_hex_str(vector : &Vec<u8>) -> String {
+    vector.iter().map(|x| format!("{x:0>2x} ")).collect::<String>()
 }
 
-pub fn unspaced_u8_vec_to_hex_str(vector : Vec<u8>) -> String {
-    vector.iter().map(|x| format!("{x:0>2b}")).collect::<String>()
+pub fn unspaced_u8_vec_to_hex_str(vector : &Vec<u8>) -> String {
+    vector.iter().map(|x| format!("{x:0>2x}")).collect::<String>()
 }
