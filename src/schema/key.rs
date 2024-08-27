6e8b4ccaaa5ec22aa7c608a74c2980797d997642
EduardoStarZ <pathver@gmail.com>
2024-08-27 16:08:54 -0300

    somewhy the key and auth schema files were deleted during the previuos diesel operation

--- src/schema/key.rs
+++ src/schema/key.rs
@@ -1,2 +1,8 @@
-// @generated automatically by Diesel CLI.
-
+diesel::table! {
+    key (id) {
+        id -> Integer,
+        user_id -> Integer,
+        public_key -> Text,
+        private_key -> Text,
+    }
+}
