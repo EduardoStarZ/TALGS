5adac0a37b60ed6b13fe3586a3e61e0903278cd4
EduardoStarz <pathver@gmail.com>
2024-09-07 16:27:14 -0300

    rechanged auth.sqlite3 to not support groups as a separate table

--- src/schema/auth.rs
+++ src/schema/auth.rs
@@ -1,25 +1,11 @@
 // @generated automatically by Diesel CLI.
 
-// @generated automatically by Diesel CLI.
-
-diesel::table! {
-    groups (id) {
-        id -> Integer,
-        name -> Text,
-    }
-}
-
 diesel::table! {
     users (id) {
-        id -> Integer,
+        id -> SmallInt,
         name -> Text,
         password -> Text,
         group -> Integer,
         email -> Text,
     }
 }
-
-diesel::allow_tables_to_appear_in_same_query!(
-    groups,
-    users,
-);
