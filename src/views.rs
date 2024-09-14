5930c136f611c11293a93aa1ce14aa82675cf61b
EduardoStarZ <pathver@gmail.com>
2024-09-14 10:33:58 -0300

    added memory usage monitor, probably not in the best place of all to monitor things tho

--- src/views.rs
+++ src/views.rs
@@ -13,9 +13,12 @@
 use ntex::web;
 
 use crate::colors::color::Color;
+use peak_alloc::PeakAlloc;
 
 pub mod auth;
 pub mod app;
+
+static PEAK : PeakAlloc = PeakAlloc;
 
 pub fn reqwestify(request: web::HttpRequest) {
     let headers : String = request.headers().iter().map(|x| format!("\t{:?} : {:?}\n", x.0, x.1)).collect::<String>();
@@ -23,4 +26,7 @@ pub fn reqwestify(request: web::HttpRequest) {
     let reqwestified : String = format!("Request Type: {:?} | URI: {:?}\n Request Headers: \n{headers}\n", request.method(), request.uri());
 
     println!("{}", reqwestified.request());
+    let usage : (String, String) = (format!("Current Memory Usage: {}", PEAK.current_usage_as_kb()), format!("Maximum Memory Usage: {}", PEAK.peak_usage_as_kb()));
+
+    println!("{} | {}", usage.0.warning(), usage.1.warning());
 }
