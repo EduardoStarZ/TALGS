7805329e8973c1642a631822ec783cc51b914fd1
EduardoStarZ <pathver@gmail.com>
2024-09-30 11:53:21 -0300

    added file path support for images into products table to allow for products to use proper images

--- migrations/app/2024-08-19-183907_created_app/up.sql
+++ migrations/app/2024-08-19-183907_created_app/up.sql
@@ -7,7 +7,8 @@ CREATE TABLE `category`(
 CREATE TABLE `product`(
 	`id` INT4 NOT NULL PRIMARY KEY,
 	`name` TEXT NOT NULL,
-	`price` FLOAT NOT NULL,
+    `image_path` TEXT, 
+    `price` FLOAT NOT NULL,
 	`id_category` INT2 NOT NULL,
 	`total_amount` INT4 NOT NULL
 );
