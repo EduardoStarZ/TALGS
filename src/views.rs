4f55a2d34bd36e57f8579073bd50bf184633eb41
EduardoStarZ <pathver@gmail.com>
2024-10-01 16:46:31 -0300

    payloads fuckn suck

--- src/views.rs
+++ src/views.rs
@@ -11,15 +11,18 @@
  * */
 
 use ntex::web;
+use ntex::util::BytesMut;
+use futures::StreamExt;
 use crate::colors::color::Color;
 use peak_alloc::PeakAlloc;
 use askama::Template;
-use serde::Deserialize;
+use serde::{Deserialize, Serialize};
 
 pub mod auth;
 pub mod app;
 
 static PEAK : PeakAlloc = PeakAlloc;
+const MAX_FILE_SIZE : usize = 5_000_000;
 
 pub fn reqwestify(request: web::HttpRequest) {
     let headers : String = request.headers().iter().map(|x| format!("\t{:?} : {:?}\n", x.0, x.1)).collect::<String>();
@@ -44,16 +47,28 @@ pub async fn file_sender(request : web::HttpRequest) -> web::HttpResponse {
     return web::HttpResponse::Ok().body(FileFormTemplate{}.render().unwrap());
 }
 
-#[derive(Deserialize)]
+#[derive(Serialize, Deserialize)]
 struct FileForm {
     path: String,
 }
 
 #[web::post("/files")]
-pub async fn file_receiver(request : web::HttpRequest, form : web::types::Form<FileForm>) -> web::HttpResponse {
+pub async fn file_receiver(request : web::HttpRequest, mut payload : web::types::Payload) -> web::HttpResponse {
     reqwestify(request);
 
-    println!("{}", form.path);
+    let mut body = BytesMut::new();
+    
+    while let Some(chunk) = payload.next().await {
+        let chunk = chunk.unwrap();
+
+        if (body.len() + chunk.len()) > MAX_FILE_SIZE {
+            return web::HttpResponse::BadRequest().body("File size too big.");
+        }
+
+        body.extend_from_slice(&chunk);
+    }
+
+    let _ : FileForm = serde_json::from_slice::<FileForm>(&body).unwrap();
 
     return web::HttpResponse::Ok().finish();
 }
