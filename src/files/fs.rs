d0beda4379429d589a20da4875afea3022365409
EduardoStarZ <pathver@gmail.com>
2024-10-03 12:49:19 -0300

    added some funcs to fs.rs

--- src/files/fs.rs
+++ src/files/fs.rs
@@ -9,3 +9,54 @@
  * TALGS is distributed under the GNU General Public license v2, see LICENSE for details
  * 
  * */
+
+use std::fs;
+
+use crate::colors::color::Color;
+
+pub enum FileExt {
+    PNG,
+    SVG,
+    JPEG
+}
+
+pub struct Image {
+    pub name : String,
+    pub content : Vec<u8>,
+}
+
+pub trait Constructor {
+    fn new() -> Image;
+}
+
+impl Constructor for Image {
+    fn new() -> Image {
+        return Image{ name: String::new(), content: Vec::new()};
+    }
+}
+
+pub fn detect_file_extension(file : Image) -> Option<FileExt> {
+    let ext : Vec<String> = file.name.split(".").map(|x| x.to_string()).collect::<Vec<String>>();
+
+    return match ext[ext.len()-1].as_str() {
+        "png" => Some(FileExt::PNG),
+        "svg" => Some(FileExt::SVG),
+        "jpg" | "jpeg" => Some(FileExt::JPEG),
+        _ => None
+    }
+}
+
+pub fn write_contents(bytes : Vec<u8>, filename : String) -> bool {
+    if fs::exists(&filename).unwrap() {
+        return false;
+    }
+
+    return match fs::write(format!("/img/{filename}").as_str(), bytes) {
+        Ok(_) => true,
+        Err(err) => {
+            println!("{}", err.to_string().warning());
+            false
+        }
+    };
+
+} 
