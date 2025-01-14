20b6cc58a9f0702d50b442057959724b3e74563e
EduardoStarZ <pathver@gmail.com>
2025-01-14 14:43:54 -0300

    created a trait to parse naive datetime to string

--- src/str/date.rs
+++ src/str/date.rs
@@ -1,7 +1,11 @@
-use chrono::{NaiveDate, NaiveDateTime};
+use chrono::{Datelike, NaiveDate, NaiveDateTime, Timelike};
 
 pub trait FromStr {
     fn from_str(str : &str) -> NaiveDateTime;
+}
+
+pub trait FromNDT {
+    fn from_naive_datetime(ndv : &NaiveDateTime) -> String;
 }
 
 impl FromStr for NaiveDateTime {
@@ -14,6 +18,12 @@ impl FromStr for NaiveDateTime {
         let hms : Vec<i32> = ymd_hms[1].split("-").map(|char| char.parse::<i32>().unwrap()).collect::<Vec<i32>>();
 
         return NaiveDate::from_ymd_opt(ymd[0], ymd[1] as u32, ymd[2] as u32).unwrap().and_hms_opt(hms[0] as u32, hms[1] as u32, hms[2] as u32).unwrap();
+    }
+}
+
+impl FromNDT for String {
+    fn from_naive_datetime(ndt : &NaiveDateTime) -> String {
+        return format!("{}-{}-{}:{}-{}-{}", ndt.year(), ndt.month(), ndt.day(), ndt.hour(), ndt.minute(), ndt.second());
     }
 }
 
